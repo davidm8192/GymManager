dcfcb685bbb33805b1e947c5ca1f5fb97cdbc5fc
David Ma <david.ma.8192@gmail.com>
2022-09-29 17:50:25 -0400

    Added initial classes

---
+++ src/Member.java
@@ -0,0 +1,14 @@
+public class Member implements Comparable<Member>{
+    private String fname;
+    private String lname;
+    private Date dob;
+    private Date expire;
+    private Location location;
+
+    @Override
+    public String toString() { }
+    @Override
+    public boolean equals(Object obj) { }
+    @Override
+    public int compareTo(Member member) { }
+}
