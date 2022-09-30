b38d36e252e756b1163f7885292cc11bc4c14eb9
Ethan Kwok <ekwok822@gmail.com>
2022-09-30 00:00:11 -0400

    Changes by david

--- src/Date.java
+++ src/Date.java
@@ -2,8 +2,39 @@ public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
-    public Date() {} //create an object with today’s date (see Calendar class)
-    public Date(String date) {} //take “mm/dd/yyyy” and create a Date object
+
+    public Date() {
+
+    } //create an object with today’s date (see Calendar class)
+
+    public Date(String date) {
+        int tempNum, count = 0;
+        String temp = "";
+
+        for(int i = 0; i < date.length(); i++) {
+            char c = date.charAt(i);
+            if(c == '/') {
+                tempNum = Integer.parseInt(temp);
+                temp = "";
+                if(count == 0) {
+                    month = tempNum;
+                    count++;
+                }
+                else if(count == 1) {
+                    day = tempNum;
+                    count++;
+                }
+                else {
+                    year = tempNum;
+                }
+            }
+            else {
+                temp += c;
+            }
+        }
+
+    } //take “mm/dd/yyyy” and create a Date object
+
     @Override
     public int compareTo(Date date) { }
     public boolean isValid() { } //check if a date is a valid calendar date
