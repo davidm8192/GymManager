388e1a8dc4b2483fca81226c27477db6fcf385d4
David Ma <david.ma.8192@gmail.com>
2022-10-01 17:06:13 -0400

    Updated run method in GymManager

--- src/GymManager.java
+++ src/GymManager.java
@@ -2,70 +2,86 @@ import java.util.Scanner;
 
 public class GymManager {
 
+    private MemberDatabase database;
+
+    public GymManager() {
+        database = new MemberDatabase();
+    }
+
     public void run() {
         Scanner sc = new Scanner(System.in);
         String line = "";
         int counter = 0;
-
-        MemberDatabase database = new MemberDatabase();
+        boolean run = true;
 
         while ((line = sc.nextLine()) != null) { //TEMP continuously read command lines so that it works with >1 command line input
             String[] words = line.split(" ");
-
-            switch (words[0]) {
-                case "A": {
-                    // Add member
-                    addMember(database, words);
-                    break;
-                }
-                case "R": {
-                    // Delete member
-                    break;
-                }
-                case "P": {
-                    // Display list of members without sorting
-                    break;
-                }
-                case "PC": {
-                    // Display list of members ordered by county names, then zip codes if the locations are in the same county
-                    break;
-                }
-                case "PN": {
-                    // Display list of members ordered by last names, then first names if last names are the same
-                    break;
-                }
-                case "PD": {
-                    // Display list of members ordered by expiration dates; if same, order does not matter
-                    break;
-                }
-                case "S": {
-                    // Display fitness class schedule
-                    break;
-                }
-                case "C": {
-                    // Check in
-                    // Do not allow check-in:
-                    // the membership has expired
-                    // the member does not exist
-                    // the date of birth is invalid
-                    // the fitness class does not exist
-                    // there is a time conflict with other fitness classes
-                    // the member has already checked in
-                    break;
-                }
-                case "Q": {
-                    // Stop the program execution and display "Gym Manager terminated.",
-                    break;
-                }
-                default: {
-                    System.out.println(words[0] + " is an invalid command!");
-                    break;
-                }
+            if(run) {
+                run = readCommand(words);
             }
+            else {
+                break;
+            }
+        }
+
+        sc.close();
+    }
+
+    private boolean readCommand(String[] words) {
+        switch (words[0]) {
+            case "A": {
+                // Add member
+                addMember(database, words);
+                break;
+            }
+            case "R": {
+                // Delete member
+                break;
+            }
+            case "P": {
+                // Display list of members without sorting
+                break;
+            }
+            case "PC": {
+                // Display list of members ordered by county names, then zip codes if the locations are in the same county
+                break;
+            }
+            case "PN": {
+                // Display list of members ordered by last names, then first names if last names are the same
+                break;
+            }
+            case "PD": {
+                // Display list of members ordered by expiration dates; if same, order does not matter
+                break;
+            }
+            case "S": {
+                // Display fitness class schedule
+                break;
+            }
+            case "C": {
+                // Check in
+                // Do not allow check-in:
+                // the membership has expired
+                // the member does not exist
+                // the date of birth is invalid
+                // the fitness class does not exist
+                // there is a time conflict with other fitness classes
+                // the member has already checked in
+                break;
+            }
+            case "Q": {
+                // Stop the program execution and display "Gym Manager terminated.",
+                return false;
+            }
+            default: {
+                System.out.println(words[0] + " is an invalid command!");
+                break;
+            }
+            return true;
         }
     }
 
-    public static void addMember(MemberDatabase database, String[] memberInfo) {
+    private void addMember(MemberDatabase database, String[] memberInfo) {
         Member m = new Member();
         m.setName(memberInfo[1], memberInfo[2]);
 
@@ -99,23 +115,23 @@ public class GymManager {
         database.add(m);
     }
 
-    public static void deleteMember(String[] memberInfo) {
+    private void deleteMember(String[] memberInfo) {
 
     }
 
-    public static void displayMembers() {}
+    private void displayMembers() {}
 
-    public static void displayByCounty() {}
+    private void displayByCounty() {}
 
-    public static void displayByName() {}
+    private void displayByName() {}
 
-    public static void displayByDate() {}
+    private void displayByDate() {}
 
-    public static void displayFitnessSched() {}
+    private void displayFitnessSched() {}
 
-    public static void checkIn() {}
+    private void checkIn() {}
 
-    public static void terminate() {}
+    private void terminate() {}
 
 
 
