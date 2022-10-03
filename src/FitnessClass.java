46bf12ebac3361d76385320469521814f9591a45
David Ma <david.ma.8192@gmail.com>
2022-10-03 03:16:31 -0400

    Fitness class

--- src/FitnessClass.java
+++ src/FitnessClass.java
@@ -1,20 +1,13 @@
 public class FitnessClass {
-    private String className;
-    private String instructorName;
-    private Time time;
+    private MemberDatabase pilates;
+    private MemberDatabase spinning;
+    private MemberDatabase cardio;
 
-    public FitnessClass(String className, String instructorName, Time time) {
-        this.className = className;
-        this.instructorName = instructorName;
-        this.time = time;
+    public FitnessClass() {
+        pilates = new MemberDatabase();
+        spinning = new MemberDatabase();
+        cardio = new MemberDatabase();
     }
     
-    /*public String getClassName() {}
-    
-    public String getInstructor() {}
-    
-    public Time getTime() {}*/
-
-
 
 }
---
+++ src/FitnessClasses.java
@@ -0,0 +1,24 @@
+public enum FitnessClasses {
+
+    PILATES("Jennifer", Time.MORNING),
+    SPINNING("Denise", Time.AFTERNOON),
+    CARDIO("Kim", Time.AFTERNOON);
+
+    private String instructorName;
+    private Time time;
+
+    FitnessClasses(String instructorName, Time time) {
+        this.instructorName = instructorName;
+        this.time = time;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+
+}
